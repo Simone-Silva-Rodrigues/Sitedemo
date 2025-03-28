7cfd154339128d6707715e608b07668d342ff415
martamarinho <martamarinhoo@hotmail.com>
2025-03-27 23:33:47 -0300

    adicionando tempo

--- cypress/e2e/compras.cy.js
+++ cypress/e2e/compras.cy.js
@@ -4,6 +4,7 @@ describe('Testando pagina de compra', () => {
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.wait(4000)
         cy.get('.product_label').should('contain', 'Products')
         cy.get('.product_label').click()
         cy.get(':nth-child(1) > .pricebar > .btn_primary').click()
