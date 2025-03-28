d24ea182cf7f2d8635cd672337adc5cf6fc9309a
martamarinho <martamarinhoo@hotmail.com>
2025-03-28 08:53:40 -0300

    implementacoes de validacoes de carrinho

--- cypress/e2e/compras.cy.js
+++ cypress/e2e/compras.cy.js
@@ -4,12 +4,12 @@ describe('Testando pagina de compra', () => {
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
-        cy.wait(4000)
-        cy.get('.product_label').should('contain', 'Products')
-        cy.get('.product_label').click()
-        cy.get(':nth-child(1) > .pricebar > .btn_primary').click()
-        cy.get(':nth-child(2) > .pricebar > .btn_primary').click()
-        cy.get(':nth-child(3) > .pricebar > .btn_primary').click()
+        cy.get('[data-test="title"]').should('contain','Products')
+        //cy.get('.product_label').should('contain', 'Products')
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        cy.get('[data-test="shopping-cart-link"]').should('contain','3')
         
     });
     
